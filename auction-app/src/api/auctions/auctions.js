852a5d886955336a1688049f74d5e49fc979545f
Pixelzzz <sherzik99@gmail.com>
2024-05-06 03:30:55 +0500

    need configurate image upload from json

--- auction-app/src/api/auctions/auctions.js
+++ auction-app/src/api/auctions/auctions.js
@@ -11,14 +11,47 @@ export async function getListing(id) {
   return response.data;
 }
 
-export async function createListing(listing) {
-  const response = await fetch(`${BASE_URL}/listings`, {
-    method: "POST",
+export async function getListingsByUser(id, token) {
+  const response = await axios.get(`${BASE_URL}/auction/listings/user/${id}`, {
     headers: {
-      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
-    body: JSON.stringify(listing),
   });
+  return response.data;
+}
+
+export async function createListing(listing, token) {
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${token}`,
+  };
+
+  const responseProduct = await axios.post(
+    `${BASE_URL}/auction/add_product/`,
+    {
+      name: listing.name,
+      description: listing.description,
+      image: listing.image,
+      initial_price: listing.initial_price,
+    },
+    {
+      headers: headers,
+    }
+  );
+
+  const response = await axios.post(
+    `${BASE_URL}/auction/create/`,
+    {
+      start_time: listing.start_time,
+      end_time: listing.end_time,
+      product: responseProduct.data.id,
+      current_price: listing.initial_price,
+      seller: listing.seller,
+    },
+    {
+      headers,
+    }
+  );
   const data = await response.json();
   return data;
 }
---
+++ auction-app/src/components/MyCabinetPage/MyCabinetPage.jsx
@@ -0,0 +1,131 @@
+import { useEffect, useState } from "react";
+import "./MyCabinetPage.css";
+import { createListing, getListingsByUser } from "../../api/auctions/auctions";
+import Button from "../Button/Button";
+import { Link } from "react-router-dom";
+import { Modal } from "react-bootstrap";
+
+export default function MyCabinetPage() {
+  const [myListings, setMyListings] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+
+  const baseUrl = "http://localhost:8000";
+  useEffect(() => {
+    const fetchListings = async () => {
+      try {
+        const user_id = localStorage.getItem("user_id");
+        const token = localStorage.getItem("access_token");
+        const listings = await getListingsByUser(user_id, token);
+        console.log(listings);
+        setMyListings(listings);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchListings();
+  }, []);
+
+  const addListing = (e) => {
+    console.log("HEdRE");
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+
+    const listing = {
+      name: formProps.name,
+      description: formProps.description,
+      image: formProps.image,
+      start_time: formProps.start_time,
+      end_time: formProps.end_time,
+      initial_price: formProps.initial_price,
+      seller: localStorage.getItem("user_id"),
+    };
+
+    const token = localStorage.getItem("access_token");
+    const response = createListing(listing, token);
+    setMyListings([...myListings, response]);
+  };
+
+  return (
+    <div className="my-cabinet-page">
+      <h1>My Personal Cabinet</h1>
+      <Button
+        variant={"light"}
+        className="my-cabinet-create-listing"
+        onClick={() => setShowModal(true)}
+      >
+        <span>Create Listing</span>
+      </Button>
+      <h2>My Listings</h2>
+      <div className="my-cabinet-listings">
+        {myListings.length === 0 ? (
+          <p>You don't have any listings yet</p>
+        ) : (
+          myListings.map((listing) => (
+            <div className="my-cabinet-listing" key={listing.id}>
+              <h3>{listing.product.name}</h3>
+              <img
+                src={baseUrl + listing.product.image}
+                alt={listing.product.name}
+              ></img>
+              <p className="my-cabinet-description">
+                {listing.product.description}
+              </p>
+
+              <div className="my-cabinet-buttons-settings">
+                <Button variant={"light"} className="my-cabinet-update-listing">
+                  <span>Update</span>
+                </Button>
+
+                <Button variant={"light"} className="my-cabinet-delete-listing">
+                  <span>Delete</span>
+                </Button>
+              </div>
+              <p>{listing.current_price}</p>
+            </div>
+          ))
+        )}
+      </div>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="my-cabinet-form" onSubmit={addListing}>
+            <input type="text" placeholder="Name" name="name" required />
+            <textarea
+              placeholder="Description"
+              name="description"
+              required
+            ></textarea>
+            <input
+              type="text"
+              placeholder="Initial price"
+              name="initial_price"
+              required
+            />
+            <input
+              type="datetime-local"
+              placeholder="Start time"
+              name="start_time"
+              required
+            />
+            <input
+              type="datetime-local"
+              placeholder="End time"
+              name="end_time"
+            />
+            <input type="file" placeholder="Image" name="image" required />
+            <button>Save Changes</button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
